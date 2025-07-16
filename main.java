b8e878947f0e0cb82f3d562bed7cbdde1965ef0a
6721601141nutthapong <nutthapong.a@ku.th>
2025-07-17 05:54:57 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    /**
+     * Entry point for the program.
+     * Runs the manual test.
+     * @param args The command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        ShoppingCartManualTest.run();
+    }
+}
+
